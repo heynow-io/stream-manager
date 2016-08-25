cd14facc211ff02b1f42f5babd7b043442187516
Mariusz Luciow <mariusz.luciow@ocado.com>
2016-08-25 08:40:38 +0200

    init

---
+++ src/main/java/io/heynow/StreamManagerApplication.java
@@ -0,0 +1,12 @@
+package io.heynow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamManagerApplication.class, args);
+	}
+}
